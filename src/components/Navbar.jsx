9ad4db8cd5c7e9ed73e83207d1b3543d2b94162e
chanminswe <chanswe67@gmail.com>
2025-01-02 17:10:28 +0630

    building my portfolio first commit
    zzzz

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Navbar.css";
+
+function Navbar() {
+  return <div className="nav-container">
+    <p>Chan Min Swe</p>
+    <p>About</p>
+    <p>Projects</p>
+  </div>;
+}
+
+export default Navbar;
