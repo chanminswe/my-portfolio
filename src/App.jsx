f1e39837115211591c4bd82e1d1516fd96d34ed8
chanminswe <chanswe67@gmail.com>
2025-01-10 11:09:18 +0630

    added project page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Introduction from "./components/Introduction";
 import TechStacks from "./components/TechStacks";
+import Projects from "./components/Projects";
 
 function App() {
   return (
@@ -9,6 +10,11 @@ function App() {
       <Navbar />
       <Introduction />
       <TechStacks />
+      <h2 style={{ width: "100%", textAlign: "center", color: "orange" , marginTop: "30px" }}>
+        My Projects
+      </h2>
+
+      <Projects />
     </>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,57 @@
-import React from 'react';
-import '../styles/projects.css';
+import React from "react";
+import "../styles/projects.css";
 
 function Projects() {
   return (
-    <div className='projects-container'>
-        <div className='project'>
-
+    <div className="projects-container">
+      <div className="project">
+        <img
+          className="project-image"
+          src="./images/exam-manager.png"
+          alt="project-1"
+        />
+        <p className="project-paragraph">
+          A web app for admins and students. Admins can create questions, manage
+          users, and view results. Students can log in, take exams, see their
+          results, and review answers. Built with React, Node.js, Express,
+          MongoDB and Tailwind CSS. Authentication is secure with JWT and
+          cookies.
+        </p>
+        <div className="project-buttons">
+          <button
+            onClick={() => window.open("")}
+            className="project-live-button"
+          >
+            Live
+          </button>
+          <button className="project-github-button">GitHub</button>
         </div>
-         
+      </div>
+      <div className="project">
+        <img
+          className="project-image"
+          src="./images/alex-vtubers.png"
+          alt="project-1"
+        />
+        <p className="project-paragraph">
+          A visually appealing and responsive portfolio showcasing an artist's
+          work. Built with React, HTML, and CSS, it uses lazy loading for
+          optimized performance and smooth image scrollers for an engaging user
+          experience. Designed with a mobile-first approach to ensure seamless
+          viewing on all devices.
+        </p>
+        <div className="project-buttons">
+          <button
+            onClick={() => window.open("")}
+            className="project-live-button"
+          >
+            Live
+          </button>
+          <button className="project-github-button">GitHub</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
